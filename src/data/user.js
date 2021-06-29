045d0c3a5b4f6c1f771b01bf43cb8b974becb0ad
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-06-29 11:39:44 -0400

    adding multiple files

--- src/data/user.js
+++ src/data/user.js
@@ -4,3 +4,8 @@ const city = "New York";
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+export {username, city, image}
+
+///export default - exporting 1 thing
+///export () - exporting more than one thing
